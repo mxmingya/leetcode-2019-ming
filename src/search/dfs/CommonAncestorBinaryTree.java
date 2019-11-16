885c80f93a8b7ad104cbfc456135c96cf3ad58bc
Ming <mxmingya@gmail.com>
2019-11-15 23:36:25 -0800

    Create CommonAncestorBinaryTree.java

---
+++ src/search/dfs/CommonAncestorBinaryTree.java
@@ -0,0 +1,32 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+class CommonAncestorBinaryTree {
+    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+        if (root == null) {
+            return root;
+        }        
+        if (root.val == p.val || root.val == q.val) {
+            return root;
+        }
+        TreeNode left = lowestCommonAncestor(root.left, p, q);
+        TreeNode right = lowestCommonAncestor(root.right, p, q);
+        
+        if (left != null && right != null) {
+            return root;
+        }
+        if (left == null && right != null) {
+            return right;
+        }
+        if (left != null && right == null) {
+            return left;
+        }
+        return null;
+    }
+}
