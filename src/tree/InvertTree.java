6dd036f7e7380b6d5c6edf13435575a8c8de27d9
Bruce Lee <brucelee@Bruces-MacBook-Air.local>
2019-11-21 17:39:30 -0800

    add tree folder

---
+++ src/tree/InvertTree.java
@@ -0,0 +1,20 @@
+
+ public class TreeNode {
+     int val;
+     TreeNode left;
+     TreeNode right;
+     TreeNode(int x) { val = x; }
+
+class InvertTree {
+    public TreeNode invertTree(TreeNode root) {
+        if (root == null) {
+            return null;
+        }
+        
+        TreeNode left = invertTree(root.left);
+        TreeNode right = invertTree(root.right);
+        root.left = right;
+        root.right = left;
+        return root;
+    }
+}
