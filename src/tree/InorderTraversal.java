056d8be9635b8956a86ba7ed4cdbe0c62a0f7955
Ming <mxmingya@gmail.com>
2019-11-29 22:33:35 -0800

    Create InorderTraversal.java

---
+++ src/tree/InorderTraversal.java
@@ -0,0 +1,26 @@
+/**
+ * Definition for a binary tree node.
+ * public class InorderTraversal {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public List<Integer> inorderTraversal(TreeNode root) {
+        List<Integer> list = new LinkedList<>();
+        dfs(root, list);
+        return list;
+    }
+    
+    void dfs(TreeNode root, List<Integer> list) {
+        if (root == null) {
+            return;
+        }
+        dfs(root.left, list);
+        list.add(root.val);
+        
+        dfs(root.right, list);
+    }
+}
