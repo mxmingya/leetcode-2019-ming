c7e896c35b06f1405da6f0e61f1e38c5f6e708a2
Ming <mxmingya@gmail.com>
2019-12-16 22:29:21 -0800

    Create ConstructBinarySearchTreefromPreorderTraversal.java

---
+++ src/tree/ConstructBinarySearchTreefromPreorderTraversal.java
@@ -0,0 +1,16 @@
+class ConstructBinarySearchTreefromPreorderTraversal {
+   int i = 0;
+    public TreeNode bstFromPreorder(int[] A) {
+        return bstFromPreorder(A, Integer.MAX_VALUE);
+    }
+
+    public TreeNode bstFromPreorder(int[] A, int bound) {
+        if (i == A.length || A[i] > bound) return null;
+        TreeNode root = new TreeNode(A[i]);
+        // System.out.printf("root: %d, bound: %d \n", A[i], bound);
+        i++;
+        root.left = bstFromPreorder(A, root.val);
+        root.right = bstFromPreorder(A, bound);
+        return root;
+    }
+}
