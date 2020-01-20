b2272590fc52eec48b018377dcd26d7b4f082bfe
Ming <mxmingya@gmail.com>
2020-01-20 00:13:03 -0800

    Update FindTownJudge.java

--- src/graph/FindTownJudge.java
+++ src/graph/FindTownJudge.java
@@ -1,13 +1,24 @@
-class FindTownJudge {
+class Solution {
     public int findJudge(int N, int[][] trust) {
         if (trust.length == 0) {
-            return -1;
+            return 1;
         }
-        Map<Integer, List<Integer>> map = new HashMap<>();
+        Map<Integer, Integer> map = new HashMap<>();
+        Set<Integer> trustPerson = new HashSet<>();
         for (int[] arr : trust) {
-            if (map.containsKey(arr[0])) {
-                
+            trustPerson.add(arr[0]);
+            if (!map.containsKey(arr[1])) {
+                map.put(arr[0], 0); 
+            }
+            map.put(arr[1], map.getOrDefault(arr[1], 0)+1);
+        }
+        
+        for (int i = 1; i <= N; i++) {
+            if (!trustPerson.contains(i) && map.containsKey(i) && map.get(i) == N - 1) {
+                return i;
             }
         }
+        
+        return -1;
     }
 }
