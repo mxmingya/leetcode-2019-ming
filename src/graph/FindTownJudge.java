d297675b5431b52bb6c9a7568fe94cc0730e8554
Ming <mxmingya@gmail.com>
2020-01-19 23:57:34 -0800

    Create FindTownJudge.java

---
+++ src/graph/FindTownJudge.java
@@ -0,0 +1,13 @@
+class FindTownJudge {
+    public int findJudge(int N, int[][] trust) {
+        if (trust.length == 0) {
+            return -1;
+        }
+        Map<Integer, List<Integer>> map = new HashMap<>();
+        for (int[] arr : trust) {
+            if (map.containsKey(arr[0])) {
+                
+            }
+        }
+    }
+}
