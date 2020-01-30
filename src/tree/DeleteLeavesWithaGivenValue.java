424da19aa8cd1ebe142e1d1233b682501f7d654f
Ming <mxmingya@gmail.com>
2020-01-30 13:58:52 -0800

    Create DeleteLeavesWithaGivenValue.java

---
+++ src/tree/DeleteLeavesWithaGivenValue.java
@@ -0,0 +1,25 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public TreeNode removeLeafNodes(TreeNode root, int target) {
+        if (root == null) {
+            return root;
+        }
+        if (root.left == null && root.right == null && root.val == target) {
+            return null;
+        }
+        root.left = removeLeafNodes(root.left, target);
+        root.right = removeLeafNodes(root.right, target);
+        if (root.left == null && root.right == null && root.val == target) {
+            return null;
+        }
+        return root;
+    }
+}
